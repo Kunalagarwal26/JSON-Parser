3787afe54d7dc17c932d5d9c3c6ce96a72f02764
Kunal Agarwal <114900074+Kunalagarwal26@users.noreply.github.com>
2026-04-07 10:35:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+def is_valid_json(input_str):
+    input_str = input_str.strip()
+    return input_str == "{}"
 
+if __name__ == "__main__":
+    test_json = input("{}: ")
+    
+    if is_valid_json(test_json):
+        print("Valid JSON")
+    else:
+        print("Invalid JSON")
